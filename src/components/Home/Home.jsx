50534b3bc0f29ab8431e631ea48b444bc9235c87
JuanLechares <juanlechares.ing@outlook.com>
2023-10-28 17:35:49 -0300

    Creacion de github

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import "./Home.css";
+
+
+const Home = () => {
+  return (
+    <>
+      <header>
+        <picture>
+          <img src={}></img>
+        </picture>
+      </header>
+    </>
+  )
+}
+
+export default Home;
