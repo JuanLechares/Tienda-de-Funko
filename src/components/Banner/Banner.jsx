59b21f115f618dbd11b388475b827c6790c43436
JuanLechares <juanlechares.ing@outlook.com>
2023-10-28 21:11:20 -0300

    Creacion de banner de home

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,11 +3,9 @@ import "./Banner.css"
 const Banner = () => {
   return (
     <section>
-        <article>
-          <h3>Nuevos Ingresos</h3>
-          <p>Descubrí el próximo Funko Pop de tu colección</p>
-          <button>shop</button>
-        </article>
+      <h3>Nuevos Ingresos</h3>
+      <p>Descubrí el próximo Funko Pop de tu colección</p>
+      <button>shop</button>
     </section>
   )
 }
